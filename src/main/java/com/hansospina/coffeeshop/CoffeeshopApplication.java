8e099d8d49d94cd5089aeec1f2433d0a36bcf0c2
Hans Ospina <hansospina@gmail.com>
2018-09-23 17:52:17 -0500

    Initial Commit

---
+++ src/main/java/com/hansospina/coffeeshop/CoffeeshopApplication.java
@@ -0,0 +1,12 @@
+package com.hansospina.coffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeshopApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hansospina/coffeeshop/CoffeeshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hansospina.coffeeshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoffeeshopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
