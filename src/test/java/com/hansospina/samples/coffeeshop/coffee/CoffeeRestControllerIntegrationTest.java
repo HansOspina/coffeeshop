14bff9ff985242026b146f124c11d82ebc9d21ec
Hans Ospina <hansospina@gmail.com>
2018-09-24 09:03:09 -0500

    Added Integration tests for Coffe REST Controller

---
+++ src/test/java/com/hansospina/samples/coffeeshop/coffee/CoffeeRestControllerIntegrationTest.java
@@ -0,0 +1,54 @@
+package com.hansospina.samples.coffeeshop.coffee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CoffeeController.class)
+public class CoffeeRestControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private CoffeeService service;
+
+
+    @Test
+    public void givenEmployees_whenGetEmployees_thenReturnJsonArray()
+            throws Exception {
+
+        String id = UUID.randomUUID().toString();
+
+        Coffee tinto = new Coffee(id, CoffeeType.SOFT, CoffeeOrigin.ANTIOQUIA, "Tinto");
+
+        List<Coffee> allEmployees = Collections.singletonList(tinto);
+
+        given(service.list()).willReturn(allEmployees);
+
+        mvc.perform(get("/api/coffee/v1/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(tinto.getName())));
+    }
+
+
+}
